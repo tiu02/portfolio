3bc66615732d1b8735dbbdac0901f7d8bac502dd
Tiffany Van <ti831318@ucf.edu>
2024-04-24 23:37:42 -0400

    lab 8 init

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
   /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
-
-  /**
    * Mobile nav toggle
    */
   const mobileNavShow = document.querySelector('.mobile-nav-show');
